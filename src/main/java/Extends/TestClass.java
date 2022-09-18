fb09f377d7f0e78d5448a9003d20a96f5fbf9bfd
Hz <1165929365@qq.com>
2022-09-18 20:19:38 +0800

    Serial序列化

---
+++ src/main/java/Extends/TestClass.java
@@ -0,0 +1,13 @@
+package Extends;
+
+import java.io.Serializable;
+
+public class TestClass implements Serializable {
+
+    private static final long serialVersionUID = -2847901469534697585L;
+    String name;
+
+    public TestClass(String name) {
+        this.name = name;
+    }
+}
